c1f65a45a09f2893b49a7a1e02de8e6a7759392f
juliocefe <juliocesarfelixhz95@gmail.com>
2022-10-02 12:20:36 -0700

    conf: config mailhog as default for mail stuff

--- app/__init__.py
+++ app/__init__.py
@@ -4,18 +4,17 @@ from flask_mail import Mail, Message
 import os
 
 app = Flask(__name__)
-app.config["MAIL_SERVER"] = os.environ.get("MAIL_SERVER")
-app.config["MAIL_PORT"] = os.environ.get("MAIL_PORT")
-app.config["MAIL_USE_SSL"] = True
-app.config["MAIL_USERNAME"] = os.environ.get("MAIL_USERNAME")
-app.config["MAIL_PASSWORD"] = os.environ.get("MAIL_PASSWORD")
-app.config["MAIL_DEFAULT_SENDER"] = os.environ.get("MAIL_DEFAULT_SENDER")
+app.config["MAIL_SERVER"] = os.environ.get("MAIL_SERVER", "mailhog")
+app.config["MAIL_PORT"] = os.environ.get("MAIL_PORT", 1025)
+app.config["MAIL_USERNAME"] = os.environ.get("MAIL_USERNAME", "mailhog")
+app.config["MAIL_PASSWORD"] = os.environ.get("MAIL_PASSWORD", "mailhog")
+app.config["MAIL_DEFAULT_SENDER"] = os.environ.get("MAIL_PASSWORD", "test@mailhog.local")
 mail = Mail(app)
 
 @app.route("/")
 def hello_world():
     context = process_transactions()
-    msg = Message("Holis", recipients=os.environ.get("RECIPIENTS").split(","))
+    msg = Message("Holis", recipients=["customer@mailserver.domain"])
     msg.html = render_template("email.html", context=context)
     mail.send(msg)
     return "Email has ben sent!"
