e43d4b96932e8854074575ace3e70df65956a1a9
juliocefe <juliocesarfelixhz95@gmail.com>
2022-10-03 16:19:45 -0700

    refactor: add local environment settings file

--- transactions_service/app/__init__.py
+++ transactions_service/app/__init__.py
@@ -4,22 +4,18 @@ from flask_mail import Mail, Message
 import os
 
 app = Flask(__name__)
-app.config["MAIL_SERVER"] = os.environ.get("MAIL_SERVER", "mailhog")
-app.config["MAIL_PORT"] = int(os.environ.get("MAIL_PORT", 1025))
+app.config["MAIL_SERVER"] = os.environ.get("MAIL_SERVER")
+app.config["MAIL_PORT"] = int(os.environ.get("MAIL_PORT"))
 app.config["MAIL_USE_SSL"] = (os.environ.get("MAIL_USE_SSL", "False")) == "True"
-app.config["MAIL_USERNAME"] = os.environ.get("MAIL_USERNAME", "mailhog")
-app.config["MAIL_PASSWORD"] = os.environ.get("MAIL_PASSWORD", "mailhog")
-app.config["MAIL_DEFAULT_SENDER"] = os.environ.get("MAIL_DEFAULT_SENDER", "test@mailhog.local")
+app.config["MAIL_USERNAME"] = os.environ.get("MAIL_USERNAME")
+app.config["MAIL_PASSWORD"] = os.environ.get("MAIL_PASSWORD")
+app.config["MAIL_DEFAULT_SENDER"] = os.environ.get("MAIL_DEFAULT_SENDER")
 mail = Mail(app)
 
 @app.route("/")
 def hello_world():
     processor = TransactionsProcessor(file_name="transactions.csv")
-    recipients = os.environ.get(
-        "RECIPIENTS", 
-        "customer@mailserver.domain"
-    ).split(",")
-    msg = Message("Holis", recipients=recipients)
+    msg = Message("Holis", recipients=os.environ.get("RECIPIENTS").split(","))
     msg.html = render_template("email.html", context=processor.data)
     mail.send(msg)
     return "Email has ben sent!"
