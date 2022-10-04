950d55e7e5023c3545ad83fa69c0275316477c52
juliocefe <juliocesarfelixhz95@gmail.com>
2022-10-03 18:36:14 -0700

    improve: access transactions table from flask service

--- transactions_service/app/__init__.py
+++ transactions_service/app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
 from app.transactions import TransactionsProcessor
 from flask_mail import Mail, Message
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.ext.automap import automap_base
 import os
 
 app = Flask(__name__)
@@ -10,9 +12,16 @@ app.config["MAIL_USE_SSL"] = (os.environ.get("MAIL_USE_SSL", "False")) == "True"
 app.config["MAIL_USERNAME"] = os.environ.get("MAIL_USERNAME")
 app.config["MAIL_PASSWORD"] = os.environ.get("MAIL_PASSWORD")
 app.config["MAIL_DEFAULT_SENDER"] = os.environ.get("MAIL_DEFAULT_SENDER")
-mail = Mail(app)
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("SQLALCHEMY_DATABASE_URI")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 
-@app.route("/")
+mail = Mail(app)
+db = SQLAlchemy(app)
+
+Base = automap_base()
+Base.prepare(db.engine, reflect=True)
+Transaction = Base.classes.accounts_transactions
+
 def hello_world():
     processor = TransactionsProcessor(file_name="transactions.csv")
     msg = Message("Holis", recipients=os.environ.get("RECIPIENTS").split(","))
