828995325835df4419c6a09a7511acf242e67c3c
juliocefe <juliocesarfelixhz95@gmail.com>
2022-10-04 12:54:30 -0700

    db: add an extra field to store unique identifier

--- stori/stori/accounts/models.py
+++ stori/stori/accounts/models.py
@@ -1,9 +1,12 @@
 from django.db import models
+import uuid
 
 
 class Account(models.Model):
     name = models.CharField(max_length=30)
+    extra_id = models.UUIDField(default=uuid.uuid4, editable=False)
     transactions_file = models.FileField(upload_to="transactions_files/")
+
 
 # TODO remove plural name to this class
 class Transactions(models.Model):
