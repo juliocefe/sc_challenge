07d51b8cb5c9dcb287ed21094580196550b04646
juliocefe <juliocesarfelixhz95@gmail.com>
2022-10-04 11:32:40 -0700

    validation: do not allow to change account objs

--- stori/stori/accounts/admin.py
+++ stori/stori/accounts/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from .models import Account, Transactions
 
-
 @admin.register(Account)
 class AccountAdmin(admin.ModelAdmin):
-    pass
+    
+    def has_change_permission(self, request, obj=None) -> bool:
+        return False
 
 @admin.register(Transactions)
 class TransactionAdmin(admin.ModelAdmin):
